e6dd1868e780749a96b2bf412ef21e48b28b51cf
Mathilda <frknilsson@hotmail.com>
2022-02-01 00:04:13 +0100

    Finished TrippleTexting

--- src/TrippleTexting.java
+++ src/TrippleTexting.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class TrippleTexting {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        int stringLength = s.length();
+        int sentenceLength = stringLength/3;
+        String firstWord = s.substring(0,sentenceLength);
+        String secondWord = s.substring(sentenceLength,sentenceLength+sentenceLength);
+        String thirdword = s.substring(sentenceLength+sentenceLength, sentenceLength+sentenceLength+sentenceLength);
+        if(firstWord.equals(secondWord) || firstWord.equals(thirdword)){
+            System.out.println(firstWord);
+        }else if(secondWord.equals(thirdword)){
+            System.out.println(secondWord);
+        }
+    }
 }
